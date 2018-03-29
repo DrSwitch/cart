b481b63b2845ac8e468b648f11e71766694aef5f
DrSwitch <switchyndra@gmail.com>
2018-03-29 20:31:40 +0800

    трололо

--- Cart v0.0/MainWindow.xaml.cs
+++ Cart v0.0/MainWindow.xaml.cs
@@ -83,6 +83,7 @@ namespace Cart_v0._0
             dgv.AllowUserToAddRows = false;
             dgv.AllowUserToResizeRows = false;
             dgv.ReadOnly = true;
+            
             dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
             this.grid1.Children.Add(host);
         }
@@ -148,25 +149,11 @@ namespace Cart_v0._0
             datatext.Text = "";
             if (selectedCellCount > 0)
             {
-                for (int i = selectedCellCount - 1; i >= 0; i--)
+                hash = new List<string>();
+                for (int i = x1; i <= x2; i++)
                 {
-                    if (i < selectedCellCount - 1)
-                        if (dgv.SelectedCells[i].ColumnIndex != dgv.SelectedCells[i + 1].ColumnIndex)
-                        {
-                            data.Add(new entity.Data(headers[dgv.SelectedCells[i + 1].ColumnIndex], hash));
-                            hash = new List<string>();
-                            datatext.Text += "\n"+ headers[dgv.SelectedCells[i + 1].ColumnIndex].GetNameHeader() + ";\n";
-                        }
-                    dgv[dgv.SelectedCells[i].ColumnIndex, dgv.SelectedCells[i].RowIndex].Style.BackColor = System.Drawing.Color.LightGreen;
-                    hash.Add(dgv[dgv.SelectedCells[i].ColumnIndex, dgv.SelectedCells[i].RowIndex].Value.ToString());
-                    datatext.Text += dgv[dgv.SelectedCells[i].ColumnIndex, dgv.SelectedCells[i].RowIndex].Value.ToString() + ", ";
-                    if (i == 0)
-                    {
-                        data.Add(new entity.Data(headers[dgv.SelectedCells[i + 1].ColumnIndex], hash));
-                        hash = new List<string>();
-                        datatext.Text += "\n" + headers[dgv.SelectedCells[i + 1].ColumnIndex].GetNameHeader() + ";\n";
-                        
-                    }
+                    hash.Add(dgv.Rows[i].Cells[j].Value.ToString());
+                    datatext.Text += dgv.Rows[i].Cells[j].Value.ToString();
                 }
             }
         }
